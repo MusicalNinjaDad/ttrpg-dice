418de96a6a66e7ffd866ef683065e7c429a5fc82
Mike Foster <i.am.mike.foster@gmail.com>
2025-01-09 18:29:11 +0000

    calculate roll required for given number of hits

--- ttrpg_dice/manydice.py
+++ ttrpg_dice/manydice.py
@@ -3,12 +3,13 @@
 from math import comb
 
 
-def _p(numdice: int, dicetype: int, target: int, hits: int) -> int:
+def multiroll(numdice: int, dicetype: int, target: int) -> list[int]:
+    """Calculate equivalent single roll instead of rolling multiple dice."""
+    def _p(hits: int) -> int:
         misses = numdice - hits
         p_successes = (target/dicetype) ** hits
         p_fails = (1-(target/dicetype)) ** (misses)
         p_hits = p_successes * p_fails * comb(numdice, hits)
         return round(p_hits * dicetype)
-
-def multiroll(numdice: int, dicetype: int, target: int) -> list[int]:
-    """Calculate equivalent single roll instead of rolling multiple dice."""
+    probs = [_p(hits) for hits in range(numdice+1)]
+    return [sum(probs[i:]) for i, _ in enumerate(probs)]
