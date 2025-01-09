e52f67de38616a9e81432e0813f49990b79795d5
Mike Foster <i.am.mike.foster@gmail.com>
2025-01-09 18:49:07 +0000

    add test for result which includes a 0

--- tests/test_manydice.py
+++ tests/test_manydice.py
@@ -5,3 +5,6 @@ from ttrpg_dice import multiroll
 
 def test_2d100_target33():
     assert multiroll(2, 100, 33) == [100, 55, 11]
+
+def test_5d100_target33_haszeroresult():
+    assert multiroll(5, 100, 33) == [100, 86, 53, 20, 4, 0]
