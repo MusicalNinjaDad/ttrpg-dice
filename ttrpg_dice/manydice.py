469a454765c5029421d13f3bcdbaf6cd0469030e
Mike Foster <i.am.mike.foster@gmail.com>
2025-01-10 08:32:53 +0000

    add docstring

--- ttrpg_dice/manydice.py
+++ ttrpg_dice/manydice.py
@@ -4,7 +4,22 @@ from math import comb
 
 
 def lazyroll(numdice: int, dicetype: int, target: int) -> list[int]:
-    """Calculate equivalent single roll instead of rolling multiple dice."""
+    """
+    Calculate equivalent single roll instead of rolling multiple dice targetting the same success value.
+    
+    Arguments:
+        numdice: the number of identical dice to roll
+        dicetype: the number of faces on the dice to roll
+        target: the target for a successful test
+
+    Examples:
+        Instead of rolling 4d100 to see which of your (skilled) goblins hit your party with their arrows you can use:
+        ```
+        >>> from ttrpg_dice import lazyroll
+        >>> lazyroll (4, 100, 33)
+        [100, 80, 40, 11, 1]
+        ```
+    """
     def _p(hits: int) -> int:
         misses = numdice - hits
         p_successes = (target/dicetype) ** hits
