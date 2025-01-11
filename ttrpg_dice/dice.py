ecc5906d1b580b7feacf6b0eaa2471dc6fde60ee
Mike Foster <i.am.mike.foster@gmail.com>
2025-01-11 10:04:06 +0000

    document attribute

--- ttrpg_dice/dice.py
+++ ttrpg_dice/dice.py
@@ -6,3 +6,4 @@ class Dice:
     def __init__(self, faces: int) -> None:
         """Build a die."""
         self.probabilities = [None] + faces*[1/faces]
+        """List of P(result) where result is index of list. P(0) = `None`"""
