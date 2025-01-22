4c9c8faf3e786176cae05288b9b9f4b13050eb53
Mike Foster <i.am.mike.foster@gmail.com>
2025-01-22 12:56:15 +0000

    namedpools test case & inistatiation test

--- tests/manydice/test_dicepools.py
+++ tests/manydice/test_dicepools.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
 import pytest  # noqa: F401, RUF100
 
 from ttrpg_dice import Dice as d  # noqa: N813
@@ -15,178 +20,96 @@ from ttrpg_dice.manydice import PoolComparison
 # output [range on POOL between 5 and 6]
 # output [range on POOL between 7 and 8]
 
-namedpools = {
-    "2d4": 2*d(4),
-    "d6 + 2": d(6)+2,
-    "d8": d(8),
+
+@dataclass
+class PoolTestCase:
+    pools: dict[Any, d] | list[d]
+    poolsdict: dict[Any, d]
+    outcomes: dict[Any, slice]
+    chances: dict[tuple[Any, Any], float]
+    table: str
+    plotabledata: dict[str, list]
+
+
+# fmt: off
+namedpools = PoolTestCase(
+    pools={
+        "two dice plus": (2*d(3)) + 2,
+        "two dice": 2*d(4),
+        "one dice plus": d(6)+2,
+        "one dice": d(8),
+    },
+    poolsdict={
+        "two dice plus": (2*d(3)) + 2,
+        "two dice": 2*d(4),
+        "one dice plus": d(6)+2,
+        "one dice": d(8),
+    },
+    outcomes={
+        "under 4": slice(None, 5),
+        "5 or 6": slice(5,7),
+        "over 6": slice(7,None),
+    },
+    chances = {
+        ("two dice plus", "under 4"): 0.111111111111,
+        ("two dice plus", "5 or 6"): 0.555555555556,
+        ("two dice plus", "over 6"): 0.333333333333,
+        ("two dice", "under 4"): 0.375,
+        ("two dice", "5 or 6"): 0.4375,
+        ("two dice", "over 6"): 0.1875,
+        ("one dice plus", "under 4"): 0.33333333333,
+        ("one dice plus", "5 or 6"): 0.33333333333,
+        ("one dice plus", "over 6"): 0.33333333333,
+        ("one dice", "under 4"): 0.5,
+        ("one dice", "5 or 6"): 0.25,
+        ("one dice", "over 6"): 0.25,
+    },
+    table = """\
+pool             under 4    5 or 6    over 6
+two dice plus      11.11     55.56     33.33
+two dice           37.50     43.75     18.75
+one dice plus      33.33     33.33     33.33
+one dice           50.00     25.00     25.00\
+""",
+    plotabledata = {
+        "x": [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3],  # Outcomes on x
+        "y": [0, 1, 2, 3] * 4,  # Pools on y
+        "z": [0] * 16,
+        "dx": [1] * 16,
+        "dy": [1] * 16,
+        "dz": pytest.approx(
+            [
+                0.111111111111, 0.375, 0.33333333333, 0.5, # under 4
+                0.555555555556, 0.4375, 0.33333333333, 0.25, # 5 or 6
+                0.333333333333, 0.1875, 0.33333333333, 0.25, # over 6
+            ],
+        ),  # Grouped by outcomes then pools
+        "color": [
+            ("b", 0), ("b", 1), ("b", 0), ("b", 0), ("b", 0.2), ("b", 0.2), # two dice plus, under 4
+            ("g", 0), ("g", 1), ("g", 0), ("g", 0), ("g", 0.2), ("g", 0.2), # two dice, under 4
+            ("r", 0), ("r", 1), ("r", 0), ("r", 0), ("r", 0.2), ("r", 0.2), # one dice plus, under 4
+            ("c", 0), ("c", 1), ("c", 0), ("c", 0), ("c", 0.2), ("c", 0.2), # one dice, under 4
+            ("b", 0), ("b", 1), ("b", 0), ("b", 0), ("b", 0.2), ("b", 0.2), # two dice plus, 5 or 6
+            ("g", 0), ("g", 1), ("g", 0), ("g", 0), ("g", 0.2), ("g", 0.2), # two dice, 5 or 6
+            ("r", 0), ("r", 1), ("r", 0), ("r", 0), ("r", 0.2), ("r", 0.2), # one dice plus, 5 or 6
+            ("c", 0), ("c", 1), ("c", 0), ("c", 0), ("c", 0.2), ("c", 0.2), # one dice, 5 or 6
+            ("b", 0), ("b", 1), ("b", 0), ("b", 0), ("b", 0.2), ("b", 0.2), # two dice plus, over 6
+            ("g", 0), ("g", 1), ("g", 0), ("g", 0), ("g", 0.2), ("g", 0.2), # two dice, over 6
+            ("r", 0), ("r", 1), ("r", 0), ("r", 0), ("r", 0.2), ("r", 0.2), # one dice plus, over 6
+            ("c", 0), ("c", 1), ("c", 0), ("c", 0), ("c", 0.2), ("c", 0.2), # one dice, over 6
+        ],
+    },
+)
+# fmt: on
+
+PoolTests = {
+    "namedpools": namedpools,
 }
 
-unnamedpools = [2*d(4), d(6)+2, d(8)]
-unnamedpools_dict = {2*d(4): 2*d(4), d(6)+2: d(6)+2, d(8): d(8)}
-
-namedoutcomes = {
-    "under 4": slice(None, 5),
-    "5 or 6": slice(5,7),
-    "over 6": slice(7,None),
-}
-
-chances = {
-    ("2d4", "under 4"): 0.375,
-    ("2d4", "5 or 6"): 0.4375,
-    ("2d4", "over 6"): 0.1875,
-    ("d6 + 2", "under 4"): 0.33333333333,
-    ("d6 + 2", "5 or 6"): 0.33333333333,
-    ("d6 + 2", "over 6"): 0.33333333333,
-    ("d8", "under 4"): 0.5,
-    ("d8", "5 or 6"): 0.25,
-    ("d8", "over 6"): 0.25,
-}
-
-chances_bypool = {
-    "2d4": {
-        "under 4": 0.375,
-        "5 or 6": 0.4375,
-        "over 6": 0.1875,
-    },
-    "d6 + 2": {
-        "under 4": 0.33333333333,
-        "5 or 6": 0.33333333333,
-        "over 6": 0.33333333333,
-    },
-    "d8": {
-        "under 4": 0.5,
-        "5 or 6": 0.25,
-        "over 6": 0.25,
-    },
-}
-
-chances_byoutcome = {
-    "under 4": {
-        "2d4": 0.375,
-        "d6 + 2": 0.33333333333,
-        "d8": 0.5,
-    },
-    "5 or 6": {
-        "2d4": 0.4375,
-        "d6 + 2": 0.33333333333,
-        "d8": 0.25,
-    },
-    "over 6": {
-        "2d4": 0.1875,
-        "d6 + 2": 0.33333333333,
-        "d8": 0.25,
-    },
-}
-
-@pytest.fixture
-def named_pools_comparison() -> PoolComparison:
-    return PoolComparison(namedpools, namedoutcomes)
-
-@pytest.fixture
-def unnamed_pools_comparison() -> PoolComparison:
-    return PoolComparison(namedpools.values(), namedoutcomes)
-
-# ==== New API =======
-
 @pytest.mark.parametrize(
-    ["inputpool", "storedpool"],
-    [
-        pytest.param(namedpools, namedpools, id="named pools"),
-        pytest.param(unnamedpools, unnamedpools_dict, id="unnamend pools"),
-    ],
+    ["pools", "outcomes", "chances"],
+    [pytest.param(test.pools, test.outcomes, test.chances, id=testid) for testid, test in PoolTests.items()],
 )
-def test_instatiation(inputpool, storedpool):
-    comparison = PoolComparison(inputpool, namedoutcomes)
-    assert comparison.pools == storedpool
-    assert comparison.outcomes == namedoutcomes
-
-
-def test_chances_no_extra_entries(named_pools_comparison):
-    assert named_pools_comparison.chances.keys() == chances.keys()
-
-
-@pytest.mark.parametrize(
-    ["combo", "chance"],
-    [pytest.param(combo, chance) for combo, chance in chances.items()],
-)
-def test_chances(named_pools_comparison, combo, chance):
-    assert named_pools_comparison.chances[combo] == pytest.approx(chance)
-
-def test_plot_poollabels(named_pools_comparison: PoolComparison):
-    fig, ax = named_pools_comparison.plot()
-    assert [label.get_text() for label in ax.get_ymajorticklabels()] == ["2d4","d6 + 2","d8"]
-    assert list(ax.get_yticks()) == [0.5,1.5,2.5]
-
-def test_plot_outcomelabels(named_pools_comparison: PoolComparison):
-    fig, ax = named_pools_comparison.plot()
-    assert [label.get_text() for label in ax.get_xmajorticklabels()] == ["under 4", "5 or 6", "over 6"]
-    assert list(ax.get_yticks()) == [0.5,1.5,2.5]
-
-def test_plotable_data(named_pools_comparison: PoolComparison):
-    data = named_pools_comparison.plotable()
-    assert data["x"] == [0,0,0,1,1,1,2,2,2] # Outcomes on x
-    assert data["y"] == [0,1,2] * 3 # Pools on y
-    assert data["z"] == [0] * 9
-    assert data["dx"] == [1] * 9
-    assert data["dy"] == [1] * 9
-    assert data["dz"] == pytest.approx(
-        [0.375, 0.33333333333, 0.5, 0.4375, 0.33333333333, 0.25, 0.1875, 0.33333333333, 0.25],
-    )  # Grouped by outcomes then pools
-    assert data["color"] == [
-        ("b",0),("b",1),("b",0),("b",0),("b",0.2),("b",0.2),
-        ("g",0),("g",1),("g",0),("g",0),("g",0.2),("g",0.2),
-        ("r",0),("r",1),("r",0),("r",0),("r",0.2),("r",0.2),
-        ("b",0),("b",1),("b",0),("b",0),("b",0.2),("b",0.2),
-        ("g",0),("g",1),("g",0),("g",0),("g",0.2),("g",0.2),
-        ("r",0),("r",1),("r",0),("r",0),("r",0.2),("r",0.2),
-        ("b",0),("b",1),("b",0),("b",0),("b",0.2),("b",0.2),
-        ("g",0),("g",1),("g",0),("g",0),("g",0.2),("g",0.2),
-        ("r",0),("r",1),("r",0),("r",0),("r",0.2),("r",0.2),
-    ]
-
-# ==== Old API =======
-
-@pytest.mark.parametrize(
-    "pool",
-    [pytest.param(pool) for pool in namedpools],
-)
-def test_named_pools(pool, named_pools_comparison: PoolComparison):
-    assert named_pools_comparison.chances_bypool[pool] == pytest.approx(chances_bypool[pool])
-
-@pytest.mark.parametrize(
-    "die",
-    [pytest.param(die, id=testid) for testid, die in namedpools.items()],
-)
-def test_unnamed_pools(die, unnamed_pools_comparison: PoolComparison):
-    assert unnamed_pools_comparison.chances_bypool[die] == pytest.approx(chances_bypool[str(die)])
-
-@pytest.mark.parametrize(
-    "outcome",
-    [pytest.param(outcome) for outcome in namedoutcomes],
-)
-def test_outcomes(outcome, named_pools_comparison: PoolComparison):
-    assert named_pools_comparison.chances_byoutcome[outcome] == pytest.approx(chances_byoutcome[outcome])
-
-formatted = """\
-pool      under 4    5 or 6    over 6
-2d4         37.50     43.75     18.75
-d6 + 2      33.33     33.33     33.33
-d8          50.00     25.00     25.00\
-"""
-
-def test_format_named(named_pools_comparison: PoolComparison):
-    assert str(named_pools_comparison) == formatted
-
-formatted = """\
-pool      under 4    5 or 6    over 6
-2d4         37.50     43.75     18.75
-d6 + 2      33.33     33.33     33.33
-d8          50.00     25.00     25.00\
-"""
-
-def test_format_unnamed(unnamed_pools_comparison: PoolComparison):
-    assert str(unnamed_pools_comparison) == formatted
-
-# TO-DO
-#
-# Revamp the test cases to give pools names which are not same as Dice
+def test_chances(pools, outcomes, chances):
+    pool = PoolComparison(pools, outcomes)
+    assert pool.chances == pytest.approx(chances)
