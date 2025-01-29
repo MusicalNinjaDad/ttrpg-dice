a8d483cc13f36c614983a2045fe3cd4eb791259e
Mike Foster <i.am.mike.foster@gmail.com>
2025-01-29 07:49:44 +0000

    lint, fmt

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -360,8 +360,8 @@ def test_slicing(dietype, sides, probabilities):
         pass
     assert all(check), msg
 
-# [0, 0.0625, 0.125, 0.1875, 0.25, 0.1875, 0.125, 0.0625]
 
+# fmt: off
 IndexTests = [
     SliceTest(
         id = "1",
@@ -394,6 +394,8 @@ IndexTests = [
         probabilities = 0.0625,
     ),
 ]
+# fmt: on
+
 
 @pytest.mark.parametrize(
     ["dietype", "side", "probability"],
